4453393bf911b3344a24f045018c85dd51e982ed
Akhil-hill <therealakn@gmail.com>
2024-08-24 15:50:39 +0530

    Update datatypes.cpp

--- datatypes.cpp
+++ datatypes.cpp
@@ -1,1 +1,12 @@
+#include<iostream>
+using namespace std;
 
+int main() {
+    int a = 12;
+    cout<<"Size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size fo float "<<sizeof(b)<<endl;
+
+    return 0;
+}
